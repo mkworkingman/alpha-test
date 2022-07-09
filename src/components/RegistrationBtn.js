86dbc44fe3feb830f8efe6baaff69400d8628e84
Maksim Kalinin <mkworkingman@gmail.com>
2022-07-09 10:41:20 +0200

    [master] Add styles for registration btn

---
+++ src/components/RegistrationBtn.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './registrationBtn.scss'
+
+export const RegistrationBtn = () => {
+  return (
+    <button className="registration-btn">Регистрация</button>
+  )
+}
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './header.scss'
 import logo from '../../images/logo.svg';
+import { RegistrationBtn } from '../RegistrationBtn';
 
 export const Header = () => {
   return (
     <header className="header">
       <nav className="container">
         <img className="logo" src={logo} alt="logo" />
+        <RegistrationBtn />
       </nav>
     </header>
   )
