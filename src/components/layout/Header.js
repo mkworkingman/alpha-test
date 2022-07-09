7c857a77d55cdbc24cdc41ecaaea0dcc4dcd6483
Maksim Kalinin <mkworkingman@gmail.com>
2022-07-09 11:04:46 +0200

    [master] Hamburger menu btn

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './header.scss'
 import logo from '../../images/logo.svg';
 import { RegistrationBtn } from '../RegistrationBtn';
 
 export const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false)
+
   return (
     <header className="header">
       <nav className="container">
-        <img className="logo" src={logo} alt="logo" />
-        <RegistrationBtn />
+        <img className="header__logo" src={logo} alt="logo" />
+        <div
+          className={
+            openMenu ? "hamburger-icon hamburger-icon--active" : "hamburger-icon"
+          }
+          onClick={() => setOpenMenu(!openMenu)}
+        >
+          <div className="hamburger-icon__lines"></div>
+        </div>
+        <div className="hamburger-menu">
+          <RegistrationBtn />
+        </div>
       </nav>
     </header>
   )
