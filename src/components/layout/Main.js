0a9dfb0bc76280d8ce5a09739d7dc1d465e30ee2
Maksim Kalinin <mkworkingman@gmail.com>
2022-07-09 13:26:30 +0200

    [master] Add page hero and add some styles to it

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { PageHero } from '../sections/PageHero'
+import { Speakers } from '../sections/Speakers'
 import './main.scss'
 
 export const Main = () => {
   return (
     <main className="main">
-      <div className="container">test</div>
+      <div className="container">
+        <PageHero />
+        <Speakers />
+      </div>
     </main>
   )
 }
---
+++ src/components/sections/PageHero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './pageHero.scss'
+
+export const PageHero = () => {
+  return (
+    <div className="pagehero">
+      <h2 className="pagehero__heading">Форум Microsoft</h2>
+      <h1 className="pagehero__heading">«Finance industry trust in AI and IT-Security»</h1>
+      <p className="pagehero__description">Масштабная кибератака и устаревшее оборудование может привести к потере клиентов, ухудшить отношения с партнёрами и учредителями, а невыполнение требований законодательства может привести к существенным штрафам и даже к блокировке ресурсов</p>
+      <p>Узнайте как этого избежать, регистрируйтесь на наш форум</p>
+    </div>
+  )
+}
