deb743b5f035d0a0542e308d48eb145a98e2a874
Maksim Kalinin <mkworkingman@gmail.com>
2022-07-09 12:28:49 +0200

    [master] Header functionality

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -11,14 +11,20 @@ export const Header = () => {
       <nav className="container">
         <img className="header__logo" src={logo} alt="logo" />
         <div
-          className={
-            openMenu ? "hamburger-icon hamburger-icon--active" : "hamburger-icon"
-          }
+          className={openMenu ? "hamburger-icon hamburger-icon--active" : "hamburger-icon"}
           onClick={() => setOpenMenu(!openMenu)}
         >
           <div className="hamburger-icon__lines"></div>
         </div>
-        <div className="hamburger-menu">
+        <div className={openMenu ? "hamburger-menu hamburger-menu--active" : "hamburger-menu"}>
+          <ul className="hamburger-menu__links-list">
+            <li className="hamburger-menu__link">
+              <a href="/">О мероприятии</a>
+            </li>
+            <li className="hamburger-menu__link">
+              <a href="/">Спикеры</a>
+            </li>
+          </ul>
           <RegistrationBtn />
         </div>
       </nav>
