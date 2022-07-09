c088e2406de519a4ae8c29167c93b00898c3070e
Maksim Kalinin <mkworkingman@gmail.com>
2022-07-09 22:29:54 +0200

    [master] Add styles for header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,27 +7,30 @@ export const Header = () => {
   const [openMenu, setOpenMenu] = useState(false)
 
   return (
-    <header className="header">
-      <nav className="container">
-        <img className="header__logo" src={logo} alt="logo" />
-        <div
-          className={openMenu ? "hamburger-icon hamburger-icon--active" : "hamburger-icon"}
-          onClick={() => setOpenMenu(!openMenu)}
-        >
-          <div className="hamburger-icon__lines"></div>
-        </div>
-        <div className={openMenu ? "hamburger-menu hamburger-menu--active" : "hamburger-menu"}>
-          <ul className="hamburger-menu__links-list">
-            <li className="hamburger-menu__link">
-              <a href="/">О мероприятии</a>
-            </li>
-            <li className="hamburger-menu__link">
-              <a href="/">Спикеры</a>
-            </li>
-          </ul>
-          <RegistrationBtn />
-        </div>
-      </nav>
-    </header>
+    <>
+      <div className="header-offset"></div>
+      <header className="header">
+        <nav className="container">
+          <img className="header__logo" src={logo} alt="logo" />
+          <div
+            className={openMenu ? "hamburger-icon hamburger-icon--active" : "hamburger-icon"}
+            onClick={() => setOpenMenu(!openMenu)}
+          >
+            <div className="hamburger-icon__lines"></div>
+          </div>
+          <div className={openMenu ? "hamburger-menu hamburger-menu--active" : "hamburger-menu"}>
+            <ul className="hamburger-menu__links-list">
+              <li className="hamburger-menu__link">
+                <a href="/">О мероприятии</a>
+              </li>
+              <li className="hamburger-menu__link">
+                <a href="/">Спикеры</a>
+              </li>
+            </ul>
+            <RegistrationBtn />
+          </div>
+        </nav>
+      </header>
+    </>
   )
 }
