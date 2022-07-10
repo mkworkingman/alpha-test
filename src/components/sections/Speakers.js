8d84c9a3cbbcc93df780f41d64d2c8901811a58a
Maksim Kalinin <mkworkingman@gmail.com>
2022-07-10 11:43:26 +0200

    [master] Add section tags

--- src/components/sections/Speakers.js
+++ src/components/sections/Speakers.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const Speakers = () => {
   return (
-    <>
+    <section>
       <h2>Спикеры нашего форума</h2>
       <h3>...filter will be here...</h3>
-    </>
+    </section>
   )
 }
