89db882fbcf339f3a78b7894cb8bc677f7449a7d
Maksim Kalinin <mkworkingman@gmail.com>
2022-07-10 16:45:48 +0200

    [master] Add some styles

--- src/components/SpeakersByDate.js
+++ src/components/SpeakersByDate.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import './speakersByDate.scss'
 
 export const SpeakersByDate = ({hour, speakers}) => {
   return (
-    <section>
-      <h3>Выступают в {hour}</h3>
-      <ul>
+    <section className="speakers-by-date">
+      <h3 className="speakers-by-date__heading">Выступают в {hour}</h3>
+      <ul className="speakers-by-date__list">
         {speakers.map(speaker => (
-          <li key={speaker.name}>{speaker.name}</li>
+          <li
+            key={speaker.name}
+            className="speaker"
+          >
+            <img className="speaker__img" src={speaker.photo_url} alt={speaker.name} />
+            {speaker.name}
+          </li>
         ))}
       </ul>
     </section>
