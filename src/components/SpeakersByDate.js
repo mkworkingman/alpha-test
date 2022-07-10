0980ab629d90a207c2ab1e41e3c4383e2ca7375a
Maksim Kalinin <mkworkingman@gmail.com>
2022-07-10 20:28:30 +0200

    [master] Add more styles for Speakers

--- src/components/SpeakersByDate.js
+++ src/components/SpeakersByDate.js
@@ -12,7 +12,9 @@ export const SpeakersByDate = ({hour, speakers}) => {
             className="speaker"
           >
             <img className="speaker__img" src={speaker.photo_url} alt={speaker.name} />
-            {speaker.name}
+            <h4 className="speaker__name">{speaker.name}</h4>
+            <p className="speaker__description">{speaker.description}</p>
+            <a href="/" className="speaker__more">Подробнее</a>
           </li>
         ))}
       </ul>
