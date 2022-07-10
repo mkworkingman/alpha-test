ea297e70a9a0534e3942f389514bdbae83114128
Maksim Kalinin <mkworkingman@gmail.com>
2022-07-10 11:31:34 +0200

    [master] Add btn to page hero

--- src/components/sections/PageHero.js
+++ src/components/sections/PageHero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { RegistrationBtn } from '../RegistrationBtn'
 import './pageHero.scss'
 
 export const PageHero = () => {
@@ -8,6 +9,7 @@ export const PageHero = () => {
       <h1 className="pagehero__heading">«Finance industry trust in AI and IT-Security»</h1>
       <p className="pagehero__description">Масштабная кибератака и устаревшее оборудование может привести к потере клиентов, ухудшить отношения с партнёрами и учредителями, а невыполнение требований законодательства может привести к существенным штрафам и даже к блокировке ресурсов</p>
       <p>Узнайте как этого избежать, регистрируйтесь на наш форум</p>
+      <RegistrationBtn />
     </div>
   )
 }
