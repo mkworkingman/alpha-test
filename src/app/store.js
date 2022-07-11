554340154e985022efb1c0fac19366442b82e0a1
Maksim Kalinin <mkworkingman@gmail.com>
2022-07-11 17:26:33 +0200

    [master] Remove initial redux content

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice';
 import filterReducer from '../features/filter/filterSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     filter: filterReducer
   },
 })
